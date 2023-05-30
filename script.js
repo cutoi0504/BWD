057468b6d0d8f4d66ace0e847d4c1680fd577081
nguyenngocchien <chienngocnguyen1234@gmail.com>
2023-05-30 15:01:23 +0700

    push

---
+++ script.js
@@ -0,0 +1,6 @@
+const menubarBtn = document.querySelector('.header-top i')
+menubarBtn.addEventListener('click', function(){
+    document.querySelector('.header-top ul').classList.toggle('active')
+
+
+})
